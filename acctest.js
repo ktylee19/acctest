787969132899c849d3ed82cacaba24746c8adc28
Katie Lee <ktylee19@gmail.com>
2014-10-04 22:45:28 -0400

    working print

--- acctest.js
+++ acctest.js
@@ -2,23 +2,23 @@ var Data = new Meteor.Collection("data");
 var tempHolder = new Meteor.Collection("tempHolder");
 
 // find average of a list
-// var average = function(myList) {
-//   var sum = 0;
-//   for(var i = 0; i < myList.length; i++) {
-//     sum += myList[i]; //don't forget to add the base
-//   }
-//   return sum/myList.length;
-// }
+var average = function(myList) {
+  var sum = 0;
+  for(var i = 0; i < myList.length; i++) {
+    sum += myList[i]; //don't forget to add the base
+  }
+  return sum/myList.length;
+}
 
 // client-side code
 if (Meteor.isClient) {
-  var ax = 0;
+  var ax = 0; // m/s^2
   var ay = 0;
-  var dx = 0;
-  var dy = 0;
-  var vx = 0;
-  var vy = 0;
-  var t = 0;
+  var dx = 0; // mm
+  var dy = 0; 
+  var vx = 0; // mm/s
+  var vy = 0; 
+  var t = 0; // ms
   var axAvg = 0;
   var ayAvg = 0;
   var sensitivity = 1;
@@ -37,47 +37,38 @@ if (Meteor.isClient) {
                } );
 
   window.addEventListener('devicemotion', function (e) {
-    // if (axList.length == 0) {
-    //   startTime = new Date().getTime();
-    //   console.log("hiere")
-    // }
-    // else {
-    //   console.log('haiiii')
-    // }
-    // ax = e.acceleration.x * sensitivity - .05;
-    // ay = e.acceleration.y * sensitivity - .05;
+    if (axList.length == 0) {
+      startTime = new Date().getTime();
+      console.log("hiere")
+    }
+    ax = e.acceleration.x * sensitivity;
+    ay = e.acceleration.y * sensitivity;
     
-    // axList.push(ax);
-    // ayList.push(ay);
-    console.log("listening");
-    console.log(axList);
-    console.log(ayList);
+    axList.push(ax);
+    ayList.push(ay);
 
-    // // 0.4 second lag 
-    // if (axList.length == 5) {
-    //   console.log("length");
-    //   axAvg = average(axList);
-    //   ayAvg = average(ayList);
-    //   endTime = new Date().getTime(); 
-    // t = endTime - startTime;
-    var tempNode = Data.findOne();
-    dx = tempNode.dx + (tempNode.vx * tempNode.t);
-    dy = tempNode.dy + (tempNode.vy * tempNode.t);
-    vx = tempNode.vx + (tempNode.ax * tempNode.t);
-    vy = tempNode.vy + (tempNode.ay * tempNode.t);
-    t = tempNode.t;
-    
-    Data.insert( { currentNode: true,
-               ax: axAvg.toFixed(2),
-               ay: ayAvg.toFixed(2),
-               vx: vx,
-               vy: vy,
-               dx: dx,
-               dy: dy,
-               t:  t  });
-    //   axList = [];
-    //   ayList = [];
-    // }
+    if (axList.length == 4) {
+      axAvg = average(axList);
+      ayAvg = average(ayList);
+      endTime = new Date().getTime(); 
+      t = endTime - startTime;
+      var tempNode = Data.findOne();
+      dx = tempNode.dx + (tempNode.vx * tempNode.t)/1000; // to get m*10^-3 instead of m^10-6
+      dy = tempNode.dy + (tempNode.vy * tempNode.t)/1000; 
+      vx = tempNode.vx + (tempNode.ax * tempNode.t); // to get mm/s
+      vy = tempNode.vy + (tempNode.ay * tempNode.t);
+      
+      Data.insert( { currentNode: true,
+                 ax: axAvg.toFixed(3),
+                 ay: ayAvg.toFixed(3),
+                 vx: vx,
+                 vy: vy,
+                 dx: dx,
+                 dy: dy,
+                 t:  t  });
+      axList = [];
+      ayList = [];
+    }
 
   } , false) ;
 
@@ -104,13 +95,7 @@ if (Meteor.isClient) {
     },
     'click #clear': function () {
       Meteor.call('removeAllData');
-    },
-    // 'click #stop': function() {
-    //   window.removeEventListener('devicemotion', listener, false);
-    // },
-    // 'click #start': function() {
-    //   window.addEventListener('devicemotion', listener, false);
-    // }
+    }
   });
 }
 
